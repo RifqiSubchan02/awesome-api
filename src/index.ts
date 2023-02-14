c4dc0d69ee0fd87d06764abbc7c91e420bb292cb
RifqiSubchan02 <rifqisubchan@gmail.com>
2023-02-15 04:50:26 +0700

    deploy

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => res.send("Welcome to My API"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Up and Running on Port ${process.env.PORT}`);
+});
